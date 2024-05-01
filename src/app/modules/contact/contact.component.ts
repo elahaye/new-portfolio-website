c14c5af7a1f98402abcd260a56c2e55eeaf26d33
elahaye <e.lahaye49@gmail.com>
2024-05-01 15:18:40 +0200

    Cleaning contact section part2

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectorRef, HostListener, inject, OnInit } from '@angular/core';
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
-import { rotatingCircle1Animation, rotatingCircle1SmallAnimation, rotatingCircle2Animation, rotatingCircle2SmallAnimation } from 'src/app/shared/animations/rotating-circle.animation';
-import { AnimationEvent } from '@angular/animations';
+import {ChangeDetectorRef, HostListener, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
+import {
+  rotatingCircle1Animation,
+  rotatingCircle1SmallAnimation,
+  rotatingCircle2Animation,
+  rotatingCircle2SmallAnimation,
+} from 'src/app/shared/animations/rotating-circle.animation';
+import {AnimationEvent} from '@angular/animations';
 
 @Component({
   standalone: true,
@@ -14,33 +19,50 @@ import { AnimationEvent } from '@angular/animations';
     rotatingCircle1Animation,
     rotatingCircle2Animation,
     rotatingCircle1SmallAnimation,
-    rotatingCircle2SmallAnimation
+    rotatingCircle2SmallAnimation,
   ],
-  providers: [
-    provideAnimations(),
-  ],
+  providers: [provideAnimations()],
   styleUrls: ['./contact.component.scss'],
   template: `
     <h1 class="grey">Contact me</h1>
 
-    <div id="structure">
-      <section class="flex flex-col place-items-center relative min-h-[40vh]">
-        <div class="main">
+    <div class="tablet:flex tablet:justify-center tablet:items-center tablet:min-h-[70vh]">
+      <section class="relative flex flex-col justify-center items-center min-h-[40vh]">
+        <div
+          class="absolute flex justify-center items-center m-auto rounded-full border border-light-grey
+          border-solid w-[230px] h-[230px] tablet:w-[330px] tablet:h-[330px]"
+        >
           <ng-container *ngIf="!isViewSizeTablet; else smallAnimation">
-            <div class="circle1" [@rotatingCircle1Animation]="rotatingState" (@rotatingCircle1Animation.done)="onEnd($event)"></div>
-            <div class="circle2" [@rotatingCircle2Animation]="rotatingState" (@rotatingCircle2Animation.done)="onEnd($event)"></div>
+            <div
+              class="circle bg-yellow"
+              [@rotatingCircle1Animation]="rotatingState"
+              (@rotatingCircle1Animation.done)="onEnd($event)"
+            ></div>
+            <div
+              class="circle bg-blue"
+              [@rotatingCircle2Animation]="rotatingState"
+              (@rotatingCircle2Animation.done)="onEnd($event)"
+            ></div>
           </ng-container>
 
           <ng-template #smallAnimation>
-            <div class="circle1" [@rotatingCircle1SmallAnimation]="rotatingState" (@rotatingCircle1SmallAnimation.done)="onEnd($event)"></div>
-            <div class="circle2" [@rotatingCircle2SmallAnimation]="rotatingState" (@rotatingCircle2SmallAnimation.done)="onEnd($event)"></div>
+            <div
+              class="circle bg-yellow"
+              [@rotatingCircle1SmallAnimation]="rotatingState"
+              (@rotatingCircle1SmallAnimation.done)="onEnd($event)"
+            ></div>
+            <div
+              class="circle bg-blue"
+              [@rotatingCircle2SmallAnimation]="rotatingState"
+              (@rotatingCircle2SmallAnimation.done)="onEnd($event)"
+            ></div>
           </ng-template>
 
           <h2 class="green">Stay in touch</h2>
         </div>
       </section>
 
-      <section class="flex flex-col place-items-center mt-20 min-h-[30vh]">
+      <section class="flex flex-col justify-center items-center min-h-[30vh]">
         <p class="grey">
           <i class="far fa-envelope"></i>
           e.lahaye49&#64;gmail.com
@@ -60,7 +82,7 @@ import { AnimationEvent } from '@angular/animations';
           </a>
         </button>
 
-        <button id="resume">
+        <button class="shadow-resume py-2.5 px-5 rounded-2xl border border-light-grey border-solid">
           <a href="assets/resume_lahaye_elise_canada.pdf" download>
             <i class="fas fa-file-download"></i>
             Download my resume
@@ -80,12 +102,12 @@ export class ContactComponent implements OnInit {
   rotatingState: string = 'in'; // Initial state
   isViewSizeTablet = false;
 
-  private cdRef = inject(ChangeDetectorRef);
+  #cdRef = inject(ChangeDetectorRef);
 
   @HostListener('window:resize')
   getScreenSize() {
     this.isViewSizeTablet = window.innerWidth < 768;
-    this.cdRef.detectChanges();
+    this.#cdRef.detectChanges();
   }
 
   ngOnInit() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      'light-green': '#e4fde1',
+      'green': '#8acb88',
+      'blue': '#648381',
+      'light-grey': '#dedee0',
+      'grey': '#4e4e57',
+      'yellow': '#ffbf46',
+    },
+    screens: {
+      'phone': '480px',
+      'tablet': '768px',
+      'large-tablet': '900px',
+      'desktop': '1024px',
+      'big-desktop': '1800px',
+    },
+    boxShadow: {
+      'resume': '3px 3px 5px var(--green-color)',
+    },
   },
   plugins: [],
-}
-
+};
